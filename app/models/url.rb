2b1bbf487b940b22cc3594f0e42721e6e8169b23
Jey Geethan <geetan123321@gmail.com>
2022-06-08 23:00:06 +0530

    Added the examples for url model. Also added the find or create class method for instantiating a shortened url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,29 @@
 # Model to contain the shortened slugs for long urls
 class Url < ApplicationRecord
+  SLUG_LENGTH = 5
+
   validates :slug, presence: true, uniqueness: true
   validates :long_url, presence: true, uniqueness: true
+
+  before_validation :generate_slug, if: :new_record?
+
+  def self.find_or_create!(long_url: '')
+    long_url.strip!
+    return nil if long_url.blank?
+
+    url = Url.where(long_url: long_url.strip).first
+    return url if url.present?
+
+    Url.create!(long_url: long_url.strip)
+  end
+
+  private
+
+  def generate_slug
+    self.slug = random_slug_string
+  end
+
+  def random_slug_string
+    [*'a'..'z', *0..9, *'A'..'Z'].shuffle[0...SLUG_LENGTH].join
+  end
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -10,11 +10,6 @@ RSpec.describe Url, type: :model do
       expect(subject).to be_valid
     end
 
-    it 'should be invalid for slug being empty' do
-      subject.slug = nil
-      expect(subject).to be_invalid
-    end
-
     it 'should be invalid for long_url being empty' do
       subject.long_url = nil
       expect(subject).to be_invalid
@@ -23,7 +18,10 @@ RSpec.describe Url, type: :model do
     it 'should be invalid for same slug' do
       subject.save!
 
-      new_url = Url.new(slug: subject.slug, long_url: 'https://www.apple.com')
+      new_url = Url.new(long_url: 'https://www.apple.com')
+      new_url.save! # autogenerate the slug
+
+      new_url.slug = subject.slug
       expect(new_url).to be_invalid
     end
 
@@ -32,6 +30,62 @@ RSpec.describe Url, type: :model do
 
       new_url = Url.new(slug: 'bnbnb', long_url: subject.long_url)
       expect(new_url).to be_invalid
+    end
+  end
+
+  describe 'Creation of Url' do
+    subject {
+      described_class.new(long_url: 'https://www.google.com')
+    }
+
+    it 'should generate a slug automatically for a given long_url' do
+      subject.save!
+
+      expect(subject.slug).to_not be_empty
+    end
+
+    it 'should generate a new unique slug for subsequent long_urls' do
+      subject.save!
+
+      new_url = Url.new(long_url: 'https://www.apple.com')
+      expect(new_url.save).to be_truthy
+      expect(new_url.slug).to_not eq(subject.slug)
+    end
+
+    it 'should generate slug with specific length' do
+      subject.save!
+      expect(subject.slug.length).to eq(Url::SLUG_LENGTH)
+    end
+  end
+
+  describe 'Find or create url' do
+    subject {
+      described_class.find_or_create!(long_url: 'https://www.apple.com')
+    }
+
+    it 'should return nil if long_url is empty' do
+      url = described_class.find_or_create!(long_url: '')
+      expect(url).to be_nil
+    end
+
+    it 'should return nil if long_url is spaces' do
+      url = described_class.find_or_create!(long_url: '   ')
+      expect(url).to be_nil
+    end
+
+    it 'should create a new url if long_url is new' do
+      expect(subject.persisted?).to be_truthy
+    end
+
+    it 'should create a new url if long_url is something else' do
+      url = described_class.find_or_create!(long_url: 'https://www.google.com')
+      expect(url).to_not be_nil
+    end
+
+    it 'should return the same url if long_url is same' do
+      url = described_class.find_or_create!(long_url: subject.long_url)
+      expect(url.slug).to eq(subject.slug)
+      expect(url).to eq(subject)
     end
   end
 end
