51243a1841df612ba7e9ec59f8ddcc9e2e145b12
Jey Geethan <geetan123321@gmail.com>
2022-06-08 22:30:45 +0530

    Added basic validations for url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,5 @@
 # Model to contain the shortened slugs for long urls
 class Url < ApplicationRecord
-
+  validates :slug, presence: true, uniqueness: true
+  validates :long_url, presence: true, uniqueness: true
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -5,8 +5,33 @@ RSpec.describe Url, type: :model do
     subject {
       described_class.new(slug: 'abcde', long_url: 'https://www.google.com')
     }
+
     it 'should be valid for valid attributes' do
       expect(subject).to be_valid
+    end
+
+    it 'should be invalid for slug being empty' do
+      subject.slug = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'should be invalid for long_url being empty' do
+      subject.long_url = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'should be invalid for same slug' do
+      subject.save!
+
+      new_url = Url.new(slug: subject.slug, long_url: 'https://www.apple.com')
+      expect(new_url).to be_invalid
+    end
+
+    it 'should be invalid for same long_url' do
+      subject.save!
+
+      new_url = Url.new(slug: 'bnbnb', long_url: subject.long_url)
+      expect(new_url).to be_invalid
     end
   end
 end
